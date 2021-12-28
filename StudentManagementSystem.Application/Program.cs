9662634d137819c08b5518a8936772938dd5489a
ismailarsln <ismail.arslan2019@bil.omu.edu.tr>
2021-12-28 16:08:15 +0300

    Add Login Form

--- StudentManagementSystem.Application/Program.cs
+++ StudentManagementSystem.Application/Program.cs
@@ -16,7 +16,7 @@ namespace StudentManagementSystem.Application
         {
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            System.Windows.Forms.Application.Run(new Form1());
+            System.Windows.Forms.Application.Run(new LoginForm());
         }
     }
 }
