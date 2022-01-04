f8c21ef4a348898b516c680a9530d29a17cbbc56
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2022-01-05 01:06:43 +0300

    Delete unused method in UniqueValueTaker

--- StudentManagementSystem.Application/Utilities/UniqueValueTaker.cs
+++ StudentManagementSystem.Application/Utilities/UniqueValueTaker.cs
@@ -7,44 +7,6 @@ namespace StudentManagementSystem.Application.Utilities
 {
     public static class UniqueValueTaker
     {
-        public static int GetUniqueValueOfSelectedItemInListbox<T>(ListBox listBox)
-        where T : class, IEntity, new()
-        {
-            int uniqueValueOfSelectedIndex;
-            // The first item selected in the listbox is of T type. The next selected items are of type int.
-            var tempObject = listBox.SelectedItem;
-
-            switch (typeof(T).Name)
-            {
-                case nameof(Department):
-                    uniqueValueOfSelectedIndex = ((Department)tempObject).DepartmentNo;
-                    break;
-                case nameof(Student):
-                    uniqueValueOfSelectedIndex = ((Student)tempObject).StudentNo;
-                    break;
-                case nameof(Officer):
-                    uniqueValueOfSelectedIndex = ((Officer)tempObject).OfficerNo;
-                    break;
-                case nameof(Instructor):
-                    uniqueValueOfSelectedIndex = ((Instructor)tempObject).InstructorNo;
-                    break;
-                case nameof(EnrolledCourse):
-                    uniqueValueOfSelectedIndex = ((EnrolledCourse)tempObject).Id;
-                    break;
-                case nameof(CatalogCourse):
-                    uniqueValueOfSelectedIndex = ((CatalogCourse)tempObject).CourseNo;
-                    break;
-                case nameof(AdviserApproval):
-                    uniqueValueOfSelectedIndex = ((AdviserApproval)tempObject).Id;
-                    break;
-                default:
-                    uniqueValueOfSelectedIndex = Convert.ToInt32(listBox.SelectedItem);
-                    break;
-            }
-
-            return uniqueValueOfSelectedIndex;
-        }
-
         public static int GetUniqueValueOfSelectedItemInComboBox<T>(ComboBox comboBox)
             where T : class, IEntity, new()
         {
