9f1d9dd866c1a0d8890ea2206e0554e6519a3cea
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2022-01-05 13:13:10 +0300

    Add exception handling to Login form

--- StudentManagementSystem.Application/LoginForm.cs
+++ StudentManagementSystem.Application/LoginForm.cs
@@ -16,66 +16,73 @@ namespace StudentManagementSystem.Application
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            var manager = new AuthenticationManager();
-            var result = manager.Login(txtGlobalUsername.Text, txtGlobalPassword.Text);
-            if (result.Success)
+            try
             {
-                if (result.Data.GetType() == typeof(Student))
+                var manager = new AuthenticationManager();
+                var result = manager.Login(txtGlobalUsername.Text, txtGlobalPassword.Text);
+                if (result.Success)
                 {
-                    var student = (Student)result.Data;
-                    MessageBox.Show($@"Hoşgeldiniz {student.FirstName} {student.LastName}.", Messages.StudentLogin);
-                    StudentForm studentForm = new StudentForm(
-                        student,
-                        this,
-                        new StudentManager(new SqlStudentDal(), new EnrolledCourseManager(new SqlEnrolledCourseDal())),
-                        new DepartmentManager(new SqlDepartmentDal()),
-                        new InstructorManager(new SqlInstructorDal(), new CatalogCourseManager(new SqlCatalogCourseDal())),
-                        new EnrolledCourseManager(new SqlEnrolledCourseDal()),
-                        new CatalogCourseManager(new SqlCatalogCourseDal()),
-                        new AdviserApprovalManager(new SqlAdviserApprovalDal())
+                    if (result.Data.GetType() == typeof(Student))
+                    {
+                        var student = (Student)result.Data;
+                        MessageBox.Show($@"Hoşgeldiniz {student.FirstName} {student.LastName}.", Messages.StudentLogin);
+                        StudentForm studentForm = new StudentForm(
+                            student,
+                            this,
+                            new StudentManager(new SqlStudentDal(), new EnrolledCourseManager(new SqlEnrolledCourseDal())),
+                            new DepartmentManager(new SqlDepartmentDal()),
+                            new InstructorManager(new SqlInstructorDal(), new CatalogCourseManager(new SqlCatalogCourseDal())),
+                            new EnrolledCourseManager(new SqlEnrolledCourseDal()),
+                            new CatalogCourseManager(new SqlCatalogCourseDal()),
+                            new AdviserApprovalManager(new SqlAdviserApprovalDal())
+                            );
+                        this.Hide();
+                        studentForm.Show();
+                    }
+                    else if (result.Data.GetType() == typeof(Instructor))
+                    {
+                        var instructor = (Instructor)result.Data;
+                        MessageBox.Show($@"Hoşgeldiniz {instructor.FirstName} {instructor.LastName}.", Messages.InstructorLogin);
+                        InstructorForm instructorForm = new InstructorForm(
+                            instructor,
+                            this,
+                            new DepartmentManager(new SqlDepartmentDal()),
+                            new InstructorManager(new SqlInstructorDal(), new CatalogCourseManager(new SqlCatalogCourseDal())),
+                            new CatalogCourseManager(new SqlCatalogCourseDal()),
+                            new StudentManager(new SqlStudentDal(), new EnrolledCourseManager(new SqlEnrolledCourseDal())),
+                            new EnrolledCourseManager(new SqlEnrolledCourseDal()),
+                            new AdviserApprovalManager(new SqlAdviserApprovalDal())
                         );
-                    this.Hide();
-                    studentForm.Show();
+                        this.Hide();
+                        instructorForm.Show();
+                    }
+                    else if (result.Data.GetType() == typeof(Officer))
+                    {
+                        var officer = (Officer)result.Data;
+                        MessageBox.Show($@"Hoşgeldiniz {officer.FirstName} {officer.LastName}.", Messages.OfficerLogin);
+                        OfficerForm officerForm = new OfficerForm(
+                            officer,
+                            new OfficerManager(new SqlOfficerDal()),
+                            new DepartmentManager(new SqlDepartmentDal()),
+                            new InstructorManager(new SqlInstructorDal(), new CatalogCourseManager(new SqlCatalogCourseDal())),
+                            new StudentManager(new SqlStudentDal(), new EnrolledCourseManager(new SqlEnrolledCourseDal())),
+                            new CatalogCourseManager(new SqlCatalogCourseDal()),
+                            this
+                        );
+                        this.Hide();
+                        officerForm.Show();
+                    }
+                    txtGlobalUsername.Clear();
+                    txtGlobalPassword.Clear();
                 }
-                else if (result.Data.GetType() == typeof(Instructor))
+                else
                 {
-                    var instructor = (Instructor)result.Data;
-                    MessageBox.Show($@"Hoşgeldiniz {instructor.FirstName} {instructor.LastName}.", Messages.InstructorLogin);
-                    InstructorForm instructorForm = new InstructorForm(
-                        instructor,
-                        this,
-                        new DepartmentManager(new SqlDepartmentDal()),
-                        new InstructorManager(new SqlInstructorDal(), new CatalogCourseManager(new SqlCatalogCourseDal())),
-                        new CatalogCourseManager(new SqlCatalogCourseDal()),
-                        new StudentManager(new SqlStudentDal(), new EnrolledCourseManager(new SqlEnrolledCourseDal())),
-                        new EnrolledCourseManager(new SqlEnrolledCourseDal()),
-                        new AdviserApprovalManager(new SqlAdviserApprovalDal())
-                    );
-                    this.Hide();
-                    instructorForm.Show();
+                    MessageBox.Show(result.Message, Messages.Error);
                 }
-                else if (result.Data.GetType() == typeof(Officer))
-                {
-                    var officer = (Officer)result.Data;
-                    MessageBox.Show($@"Hoşgeldiniz {officer.FirstName} {officer.LastName}.", Messages.OfficerLogin);
-                    OfficerForm officerForm = new OfficerForm(
-                        officer,
-                        new OfficerManager(new SqlOfficerDal()),
-                        new DepartmentManager(new SqlDepartmentDal()),
-                        new InstructorManager(new SqlInstructorDal(), new CatalogCourseManager(new SqlCatalogCourseDal())),
-                        new StudentManager(new SqlStudentDal(), new EnrolledCourseManager(new SqlEnrolledCourseDal())),
-                        new CatalogCourseManager(new SqlCatalogCourseDal()),
-                        this
-                    );
-                    this.Hide();
-                    officerForm.Show();
-                }
-                txtGlobalUsername.Clear();
-                txtGlobalPassword.Clear();
             }
-            else
+            catch (Exception exception)
             {
-                MessageBox.Show(result.Message, Messages.Error);
+                MessageBox.Show($"Bir şeyler ters gitti.\n\n{exception.Message}", Messages.Error);
             }
         }
     }
